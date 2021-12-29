8d966e57263cc6aa35491d9acc1ed4f0a249f9ad
SMJin <sstaw@naver.com>
2021-12-29 22:33:02 +0900

    my BinarySearch.java

---
+++ src/ch3/binarySearch/BinarySearch.java
@@ -0,0 +1,48 @@
+package src.ch3.binarySearch;
+
+public class BinarySearch {
+
+    static int binSearchX(int[] a, int n, int key)
+    {
+        int left=0;
+        int center = n/2;
+        int right=a.length;
+
+        int resultIdx = -1;
+
+        int i;
+        for(i=0 ; i<n ; i++)
+        {
+            if (a[center] == key)
+            {
+                resultIdx = center;
+            }
+            else if (a[center] < key)
+            {
+                right = center;
+                center = (left+right)/2;
+            }
+            else
+            {
+                left = center;
+                center = (left+right)/2;
+            }
+        }
+
+        while((resultIdx>0) && (a[resultIdx-1] == a[resultIdx]))
+        {
+            resultIdx--;
+        }
+
+        return resultIdx;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 7, 7, 7, 8, 8, 9, 9};
+        int n = array.length;
+        int key = 7;
+
+        System.out.print("array에 저장되어 있는 key값 " + key + "의 첫 인덱스 : ");
+        System.out.println(binSearchX(array, n, key));
+    }
+}
