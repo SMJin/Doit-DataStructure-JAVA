dd9c241aa183ca33446356244da9119ec3032701
SMJin <sstaw@naver.com>
2021-12-30 15:53:03 +0900

    boj_2493.java

--- src/ch4/README.md
+++ src/ch4/README.md
@@ -1,3 +1,4 @@
 # [Stack](./Stack/IntStack.java)
+### [BOJ 2493](./Stack/boj_2493.java)
 # [Queue](./Queue/IntQueue.java)
 ###### 큐는 링버퍼로 구현하였는데, 이를 사용하면 "오래된 데이터를 버리는" 용도로 사용이 가능하다.
---
+++ src/ch4/Stack/boj_2493.java
@@ -0,0 +1,85 @@
+package src.ch4.Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class boj_2493 {
+    public static void main(String[] args)
+    {
+        scan.init();
+        int size = scan.nextInt();
+        Stack<Integer> stk = new Stack<>();
+        Stack<Integer> idx = new Stack<>();
+        StringBuffer sb = new StringBuffer();
+
+        int i;
+        for (i=1; i<=size; i++)
+        {
+            int current = scan.nextInt();
+
+            while (!stk.isEmpty())
+            {
+                if (stk.peek() >= current)
+                {
+                    sb.append(idx.peek() + " ");
+                    break;
+                }
+                stk.pop();
+                idx.pop();
+            }
+            if (stk.isEmpty())
+                sb.append("0 ");
+
+            stk.push(current);
+            idx.push(i);
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    //scan을 재정의함.
+    static class scan {
+        private static BufferedReader br;
+        private static StringTokenizer st;
+
+        static void init() {
+
+            br = new BufferedReader(new InputStreamReader(System.in));
+
+            st = new StringTokenizer("");
+
+        }
+
+
+
+        static String next() {
+
+            while (!st.hasMoreTokens()) {
+
+                try {
+
+                    st = new StringTokenizer(br.readLine());
+
+                } catch (IOException e) {
+
+                }
+
+            }
+
+            return st.nextToken();
+
+        }
+
+
+
+        static int nextInt() {
+
+            return Integer.parseInt(next());
+
+        }
+
+    }
+}
