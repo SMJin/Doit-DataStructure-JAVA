54d5629d5c141a1feed31f927115b9275fe484f4
SMJin <sstaw@naver.com>
2022-01-13 15:45:35 +0900

    QuickSort :: MySolution

--- src/ch6/Quick/QuickSort.java
+++ src/ch6/Quick/QuickSort.java
@@ -2,27 +2,35 @@ package src.ch6.Quick;
 
 public class QuickSort {
 
-    public static void quickSort(int[] a) {
-        int len = a.length;
-        int pivot = a[len/2];
-        int pl = 0, pr = len-1;
+    static void quickSort(int[] a, int pl, int pr) {
+        int start = pl, end = pr;
+        int pi = (pl+pr)/2;
+        int pivot = a[pi];
+
+        showArray(a);
+        System.out.println("pivot 값은 " + pivot + "입니다.");
+        System.out.println("(pl) a[" + pl + "]: " + a[pl]);
+        System.out.println("(pr) a[" + pr + "]: " + a[pr]);
 
         do {
             while (a[pl] < pivot) pl++;
             while (a[pr] > pivot) pr--;
-            System.out.println("(pl) a[" + pl + "]: " + a[pl] +
-                    ", (pr) a[" + pr + "]: " + a[pr]);
-            if (pl <= pr) {
+            //System.out.println("a[" + pl + "]: " + a[pl] + ", a[" + pr + "]: " + a[pr]);
+            if (pl <= pr)
                 swap(a, pl++, pr--);
-            }
-        } while (pl <= pr);
+            //showArray(a);
+        } while(pl <= pr);
+
+        if (start + 1 < pr)
+            quickSort(a, start, pr);
+        if ((pr+1) + 1 < end)
+            quickSort(a, pr+1, end);
     }
 
-    private static void swap(int[] a, int i, int j) {
-        int tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
-        showArray(a);
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 
     public static void showArray(int[] array) {
@@ -32,31 +40,8 @@ public class QuickSort {
         System.out.println();
     }
 
-    public static void showArray2(int[] array) {
-        int i, len = array.length, pivot = len/2;
-        System.out.print("pivot 이하의 그룹: ");
-        for(i=0; i<pivot; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println("\npivot 값은 " + array[pivot] + "입니다.");
-        System.out.print("pivot 이상의 그룹: ");
-        for(i=pivot+1; i<len; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-
-    }
-
     public static void main(String[] args) {
-        int[] array1 = {5,7,1,4,6,2,3,9,8};
-        showArray(array1);
-        quickSort(array1);
-        showArray2(array1);
-
-        System.out.println();
-        int[] array2 = {1,8,7,4,5,2,6,3,9};
-        showArray(array2);
-        quickSort(array2);
-        showArray2(array2);
+        int[] array = {5,7,1,4,6,2,3,9,8};
+        quickSort(array, 0, array.length-1);
     }
 }
